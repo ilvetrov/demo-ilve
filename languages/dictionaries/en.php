4a123db25f5137b258df48f64b05ad50df26b967
ilvetrov <ilya.ilvetrov@gmail.com>
2021-09-08 21:37:48 +0800

    improve languages system

---
+++ languages/dictionaries/en.php
@@ -0,0 +1,50 @@
+<?php
+return [
+  "_aliases" => [],
+  "by" => "by",
+  "by_my_name" => "Ilya Vetrov",
+  "text" => "Text",
+  "demo_site" => "Demo site",
+  "demo_text" => [
+    [
+      "text" => "Here is only what we have created."
+    ],
+    [
+      "text" => "You can see the site «after us» by ",
+      "link" => "the link"
+    ]
+  ],
+  "demo_text_of_deleted" => [
+    [
+      "text" => "The site has stopped working."
+    ],
+    [
+      "text" => "But here you can look at its working version."
+    ],
+    [
+      "text" => "Previously, the site was under ",
+      "link" => "the link"
+    ]
+  ],
+  "demo_error_text" => [
+    [
+      "text" => "You cannot submit a form here. This is a demo site."
+    ],
+    [
+      "text" => "To submit the form, go to ",
+      "link" => "real site"
+    ]
+  ],
+  "hide_notification" => "Hide notification",
+  "enable_js" => "And enable JavaScript here",
+  "stop" => "Stop",
+  "we_recommend_demo" => "Client changed site",
+  "open_demo" => "Go to demo",
+  "open_site" => "Open site",
+  "we_recommend_demo_text" => [
+    [
+      "text" => "To show only our work, we uploaded it to the demo site =>"
+    ]
+  ],
+  "close" => "Close",
+];
--- languages/index.php
+++ languages/index.php
@@ -5,22 +5,30 @@ require_once PATH_PREFIX . '/inc/get-php-files.php';
 
 class Languages
 {
-  static private $current = 'en';
+  static private $default = 'ru';
+  static private $current = 'ru';
 
   static private $dictionaries_files_names = [];
   static private $dictionaries = [];
   static public function init()
   {
-    self::$dictionaries_files_names = array_diff(scandir('languages/dictionaries'), ['.', '..']);
-    foreach (self::$dictionaries_files_names as $dictionary_file_name) {
-      $code_name = implode('.', array_remove_last(explode('.', $dictionary_file_name)));
-      self::$dictionaries[$code_name] = require("dictionaries/$dictionary_file_name");
-    }
+    self::$dictionaries_files_names = get_php_files_names('/languages/dictionaries');
+    self::$dictionaries = get_php_files('/languages/dictionaries');
   }
 
   static public function set_current(String $code_name)
   {
-    self::$current = $code_name;
+    self::$current = self::validate($code_name);
+  }
+  static public function validate(String $raw_code_name_for_validating)
+  {
+    $code_name_for_validating = trim($raw_code_name_for_validating);
+    foreach (self::$dictionaries as $code_name => $dictionary) {
+      if ($code_name === $code_name_for_validating || in_array($code_name_for_validating, $dictionary['_aliases'])) {
+        return $code_name;
+      }
+    }
+    return self::$default;
   }
   static public function get(String $code_name)
   {
@@ -30,10 +38,31 @@ class Languages
   {
     return self::get(self::$current);
   }
-  static public function check(String $code_name)
+  static public function get_current_name()
   {
-    $file_name = "$code_name.php";
-    return in_array($file_name, self::$dictionaries_files_names);
+    return self::$current;
+  }
+  static public function get_default()
+  {
+    return self::get(self::$default);
+  }
+  static public function get_default_name()
+  {
+    return self::$default;
+  }
+  static public function text(String $text)
+  {
+    return self::get_current()[$text] ?? self::get_default()[$text] ?? $text;
+  }
+  static public function get_requested_name()
+  {
+    if (@$_GET['demo-lang']) return self::validate($_GET['demo-lang']);
+    return false;
   }
 }
 Languages::init();
+
+function lang(String $text)
+{
+  return Languages::text($text);
+}
